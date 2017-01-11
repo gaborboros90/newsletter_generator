3e69b5d1e3cd593211c7fb6af7cb056b51e89b79
Gabor Boros <gabor_boros@epam.com>
2017-01-11 11:16:47 +0100

    Create application model

--- app/components/app-component/appComponentController.js
+++ app/components/app-component/appComponentController.js
@@ -9,6 +9,64 @@ define(
             var vm = this;
 
             this.headerText = 'App Component';
+
+            this.model = {
+                highlight: {
+                    features: {
+                        title: 'Bigger features',
+                        list: ['']
+                    },
+                    mvts: {
+                        title: 'New MVT-s',
+                        list: ['']
+                    }
+                },
+                releaseStatistics: {
+                    finishedStories: {
+                        title: 'Finished stories',
+                        numbersByPod: [
+                            {
+                                'LPS1': 0,
+                            },
+                            {
+                                'LPS2': 0,
+                            },
+                            {
+                                'LPS3': 0,
+                            },
+                            {
+                                'LPS4': 0,
+                            }
+                        ],
+                        sum: 0
+                    },
+                    bugRelatedStatistics: {
+                        title: 'Bug related statistics',
+                        raisedBugs: 0,
+                        resolvedBug: 0
+                    }
+                },
+                anniversaries: {
+                    birthdays: {
+                        title: 'Birthdays',
+                        list: [],
+                    },
+                    landingAnniversaries: {
+                        title: 'Landing anniversaries',
+                        list: []
+                    }
+                },
+                actualThings: [
+                    {
+                        title: '',
+                        imgData: ''
+                    },
+                    {
+                        title: '',
+                        imgData: ''
+                    }
+                ]
+            }
         };
     }
 );
