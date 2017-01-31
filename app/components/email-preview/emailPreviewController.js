2879d09d22f934ebbae307453cdd2c2af89a3be6
Gabor Boros <gabor_boros@epam.com>
2017-01-31 10:51:59 +0100

    Bugfixing for table and some re-design again

--- app/components/email-preview/emailPreviewController.js
+++ app/components/email-preview/emailPreviewController.js
@@ -9,15 +9,30 @@ define(
             var vm = this;
 
             vm.getLinkText = getLinkText;
+            vm.getFirstVisibleResolutionIndex = getFirstVisibleResolutionIndex;
 
             function getLinkText(href) {
                 var urlParts;
 
-                if(typeof href === 'string') {
+                if (typeof href === 'string') {
                     urlParts = href.split('/');
 
                     return urlParts[urlParts.length - 1];
                 }
+            }
+
+            function getFirstVisibleResolutionIndex(resolutionList) {
+                var index = -1;
+
+                for (var i = 0; i < resolutionList.length; i++) {
+                    if(resolutionList[i].closedBugs > 0 || resolutionList[i].raisedBugs) {
+                        index = i;
+
+                        break;
+                    }
+                }
+
+                return index;
             }
         };
     }
